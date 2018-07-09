52f5d69b83351bad7d96395372c49d11b9544d3e
Clenio Santos <clenio.santos@taurustech.com.br>
2018-07-09 16:23:15 -0700

    ajuste na edição de agendamento

--- back/app/controllers/scheduleController.js
+++ back/app/controllers/scheduleController.js
@@ -1,6 +1,8 @@
 const Schedule = require('../models').Schedule;
 const Material = require('../models').Material;
 const Procedure = require('../models').Procedure;
+const ScheduleMaterial = require('../models').ScheduleMaterial;
+const ScheduleProcedure = require('../models').ScheduleProcedure;
 
 module.exports = {
     create(req, res) {
@@ -13,7 +15,7 @@ module.exports = {
             payment: receivedSchedule.payment,
             start: receivedSchedule.start
         }).then(newSchedule => {
-          updateAssociates(receivedSchedule,newSchedule);
+          updateAssociatesOnCreate(receivedSchedule,newSchedule);
           res.json(newSchedule).code(200);  
         }); 
     },
@@ -40,12 +42,12 @@ module.exports = {
 
     update(req,res){   
         let receivedSchedule = Object.assign({}, req.body, req.params);     
-    
+        
         Schedule.find({
             where:{ id:receivedSchedule.id }
         }).then((schedule) => {
-            schedule.update(receivedSchedule, {include: {all: true, nested:true}});            
-            updateAssociates(receivedSchedule,schedule);
+            schedule.update(receivedSchedule.sendJson, {include: {all: true, nested:true}});            
+            updateAssociatesOnUpdate(receivedSchedule,schedule);
             res.json(schedule).code(200);  
         });        
     },
@@ -58,7 +60,47 @@ module.exports = {
     },
 };
 
-function updateAssociates(receivedSchedule,schedule){ 
+function updateAssociatesOnUpdate(receivedSchedule,schedule){    
+    if(receivedSchedule.removeOnEdit.length>0){
+        receivedSchedule.removeOnEdit.forEach((d) => {
+            if(d.type=="Procedimento"){
+                ScheduleProcedure.destroy({where: 
+                    {
+                        ProcedureId: d.procedure,
+                        ScheduleId: receivedSchedule.id
+                }});
+            }
+
+            if(d.type=="Material"){
+                ScheduleMaterial.destroy({where: 
+                    {
+                        MaterialId: d.material,
+                        ScheduleId: receivedSchedule.id
+                }});
+            }
+        });    
+    }
+
+    if(receivedSchedule.sendJson.arrMaterial.length>0){
+        receivedSchedule.sendJson.arrMaterial.forEach(element => {
+            Material.findById(element.material).then((m) => {
+                schedule.addScheduleHasMaterials(m,{ through: { quantity: element.quantity_material, totalPrice: element.totalPrice }});
+            });             
+        });
+    }
+
+    
+    if(receivedSchedule.sendJson.arrProcedure.length>0){
+        receivedSchedule.sendJson.arrProcedure.forEach(element => {
+            Procedure.findById(element.procedure).then((p) => {
+                schedule.addScheduleHasProcedures(p, { through: { quantity: element.quantity_procedure, totalPrice: element.totalPrice }});
+            });
+        })
+    }
+   
+}
+
+function updateAssociatesOnCreate(receivedSchedule,schedule){ 
     if(receivedSchedule.arrMaterial.length>0){
         receivedSchedule.arrMaterial.forEach(element => {
             Material.findById(element.material).then((m) => {
@@ -67,7 +109,6 @@ function updateAssociates(receivedSchedule,schedule){
         });
     }
 
-    
     if(receivedSchedule.arrProcedure.length>0){
         receivedSchedule.arrProcedure.forEach(element => {
             Procedure.findById(element.procedure).then((p) => {
